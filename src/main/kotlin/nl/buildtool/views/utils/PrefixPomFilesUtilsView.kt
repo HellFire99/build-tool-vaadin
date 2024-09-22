4c1da5dee6297c223b2c39db9574e7f6ca49b768
rh <robheikoop@gmail.com>
2024-09-22 11:47:52 +0200

    PrefixPomFilesView added

---
+++ src/main/kotlin/nl/buildtool/views/utils/PrefixPomFilesUtilsView.kt
@@ -0,0 +1,115 @@
+package nl.buildtool.views.utils
+
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.button.ButtonVariant
+import com.vaadin.flow.component.html.H5
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.component.radiobutton.RadioButtonGroup
+import com.vaadin.flow.component.radiobutton.RadioGroupVariant
+import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.theme.lumo.LumoUtility
+import nl.buildtool.views.build.PomFileDataProvider
+import org.springframework.stereotype.Component
+
+@Component
+class PrefixPomFilesUtilsView(private val pomFileDataProvider: PomFileDataProvider) {
+    fun addMainRowToContent(utilsView: UtilsView) {
+        val mainRow = HorizontalLayout()
+        mainRow.setId("mainRow")
+
+        val leftColumn = VerticalLayout()
+        leftColumn.setId("leftColumn")
+
+        val prefixPomsButton = Button("Prefix pom files")
+        val updateDependenciesButton = Button("Update  dependencies")
+        val layoutColumn3 = VerticalLayout()
+        layoutColumn3.setId("layoutColumn3")
+
+        val h5 = H5()
+        val innerRow = HorizontalLayout()
+        innerRow.setId("innerRow")
+        innerRow.minHeight = "490px"
+
+        val middleColumn = VerticalLayout()
+        middleColumn.setId("middleColumn")
+
+        val pomFileSelectRadio = RadioButtonGroup<String>()
+        pomFileSelectRadio.setId("pomFileSelectRadio")
+
+        val customOrAutoDetectPrefixRadio = RadioButtonGroup<String>()
+        customOrAutoDetectPrefixRadio.setId("customOrAutoDetectPrefixRadio")
+
+        val customPrefixTextfield = TextField()
+        customPrefixTextfield.setId("customPrefixTextfield")
+
+        val rightColumn = VerticalLayout()
+        rightColumn.setId("rightColumn")
+
+        val treeGrid = pomFileDataProvider.createTreeGrid()
+
+        utilsView.content!!.addClassName(LumoUtility.Gap.XSMALL)
+        utilsView.content!!.addClassName(LumoUtility.Padding.XSMALL)
+        utilsView.content!!.width = "100%"
+        utilsView.content!!.style["flex-grow"] = "1"
+        mainRow.addClassName(LumoUtility.Gap.XSMALL)
+        mainRow.width = "100%"
+        mainRow.style["flex-grow"] = "1"
+        leftColumn.addClassName(LumoUtility.Padding.XSMALL)
+        leftColumn.style["flex-grow"] = "1"
+        leftColumn.width = "190px"
+        prefixPomsButton.width = "min-content"
+        prefixPomsButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
+        updateDependenciesButton.width = "min-content"
+        updateDependenciesButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
+        layoutColumn3.addClassName(LumoUtility.Gap.XSMALL)
+        layoutColumn3.addClassName(LumoUtility.Padding.XSMALL)
+        layoutColumn3.width = "100%"
+        layoutColumn3.style["flex-grow"] = "1"
+        h5.text = "Prefix pom files"
+        h5.width = "max-content"
+        innerRow.setWidthFull()
+        layoutColumn3.setFlexGrow(1.0, innerRow)
+        innerRow.addClassName(LumoUtility.Gap.XSMALL)
+        innerRow.addClassName(LumoUtility.Padding.XSMALL)
+        innerRow.width = "100%"
+        innerRow.style["flex-grow"] = "1"
+        middleColumn.setHeightFull()
+        innerRow.setFlexGrow(1.0, middleColumn)
+        middleColumn.addClassName(LumoUtility.Gap.XSMALL)
+        middleColumn.addClassName(LumoUtility.Padding.XSMALL)
+        middleColumn.width = "min-content"
+        middleColumn.style["flex-grow"] = "1"
+        pomFileSelectRadio.label = "Choose which pom files to update"
+        pomFileSelectRadio.width = "min-content"
+        pomFileSelectRadio.setItems("All in workspace", "Selection")
+        pomFileSelectRadio.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL)
+
+        customOrAutoDetectPrefixRadio.label = "Auto-detect or custom prefix"
+        customOrAutoDetectPrefixRadio.width = "min-content"
+        customOrAutoDetectPrefixRadio.setItems("Auto-detect", "Custom prefix")
+        customOrAutoDetectPrefixRadio.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL)
+        customPrefixTextfield.label = "Prefix"
+        customPrefixTextfield.width = "100%"
+        rightColumn.setHeightFull()
+        innerRow.setFlexGrow(1.0, rightColumn)
+        rightColumn.addClassName(LumoUtility.Gap.XSMALL)
+        rightColumn.addClassName(LumoUtility.Padding.XSMALL)
+        rightColumn.width = "100%"
+        rightColumn.style["flex-grow"] = "1"
+
+        utilsView.content!!.add(mainRow)
+        mainRow.add(leftColumn)
+        leftColumn.add(prefixPomsButton)
+        leftColumn.add(updateDependenciesButton)
+        mainRow.add(layoutColumn3)
+        layoutColumn3.add(h5)
+        layoutColumn3.add(innerRow)
+        innerRow.add(middleColumn)
+        middleColumn.add(pomFileSelectRadio)
+        middleColumn.add(customOrAutoDetectPrefixRadio)
+        middleColumn.add(customPrefixTextfield)
+        innerRow.add(rightColumn)
+        rightColumn.add(treeGrid)
+    }
+}
--- src/main/kotlin/nl/buildtool/views/utils/UtilsView.kt
+++ src/main/kotlin/nl/buildtool/views/utils/UtilsView.kt
@@ -4,20 +4,14 @@ import com.vaadin.flow.component.Composite
 import com.vaadin.flow.component.button.Button
 import com.vaadin.flow.component.button.ButtonVariant
 import com.vaadin.flow.component.dependency.Uses
-import com.vaadin.flow.component.html.H5
 import com.vaadin.flow.component.icon.Icon
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
-import com.vaadin.flow.component.radiobutton.RadioButtonGroup
-import com.vaadin.flow.component.radiobutton.RadioGroupVariant
-import com.vaadin.flow.component.textfield.TextField
 import com.vaadin.flow.router.Menu
 import com.vaadin.flow.router.PageTitle
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.theme.lumo.LumoUtility
-import nl.buildtool.LoggingService
 import nl.buildtool.views.MainLayout
-import nl.buildtool.views.build.PomFileDataProvider
 
 @PageTitle("Utils")
 @Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0.0)
@@ -25,10 +19,7 @@ import nl.buildtool.views.build.PomFileDataProvider
 @Uses(
     Icon::class
 )
-class UtilsView(
-    private val pomFileDataProvider: PomFileDataProvider,
-    private val loggingService: LoggingService
-) : Composite<VerticalLayout?>() {
+class UtilsView(prefixPomFilesUtilsView: PrefixPomFilesUtilsView) : Composite<VerticalLayout?>() {
 
     init {
         val footerRow = HorizontalLayout()
@@ -42,107 +33,9 @@ class UtilsView(
         executeButton.style["flex-grow"] = "1"
         executeButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
 
-        addMainRowToContent()
+        prefixPomFilesUtilsView.addMainRowToContent(this)
         footerRow.add(executeButton)
         content!!.add(footerRow)
     }
 
-    private fun addMainRowToContent() {
-        val mainRow = HorizontalLayout()
-        mainRow.setId("mainRow")
-
-        val leftColumn = VerticalLayout()
-        leftColumn.setId("leftColumn")
-
-        val prefixPomsButton = Button("Prefix pom files")
-        val updateDependenciesButton = Button("Update  dependencies")
-        val layoutColumn3 = VerticalLayout()
-        layoutColumn3.setId("layoutColumn3")
-
-        val h5 = H5()
-        val innerRow = HorizontalLayout()
-        innerRow.setId("innerRow")
-        innerRow.minHeight = "490px"
-
-        val middleColumn = VerticalLayout()
-        middleColumn.setId("middleColumn")
-
-        val pomFileSelectRadio = RadioButtonGroup<String>()
-        pomFileSelectRadio.setId("pomFileSelectRadio")
-
-        val customOrAutoDetectPrefixRadio = RadioButtonGroup<String>()
-        customOrAutoDetectPrefixRadio.setId("customOrAutoDetectPrefixRadio")
-
-        val customPrefixTextfield = TextField()
-        customPrefixTextfield.setId("customPrefixTextfield")
-
-        val rightColumn = VerticalLayout()
-        rightColumn.setId("rightColumn")
-
-        val treeGrid = pomFileDataProvider.createTreeGrid()
-
-        content!!.addClassName(LumoUtility.Gap.XSMALL)
-        content!!.addClassName(LumoUtility.Padding.XSMALL)
-        content!!.width = "100%"
-        content!!.style["flex-grow"] = "1"
-        mainRow.addClassName(LumoUtility.Gap.XSMALL)
-        mainRow.width = "100%"
-        mainRow.style["flex-grow"] = "1"
-        leftColumn.addClassName(LumoUtility.Padding.XSMALL)
-        leftColumn.style["flex-grow"] = "1"
-        leftColumn.width = "190px"
-        prefixPomsButton.width = "min-content"
-        prefixPomsButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
-        updateDependenciesButton.width = "min-content"
-        updateDependenciesButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
-        layoutColumn3.addClassName(LumoUtility.Gap.XSMALL)
-        layoutColumn3.addClassName(LumoUtility.Padding.XSMALL)
-        layoutColumn3.width = "100%"
-        layoutColumn3.style["flex-grow"] = "1"
-        h5.text = "Prefix pom files"
-        h5.width = "max-content"
-        innerRow.setWidthFull()
-        layoutColumn3.setFlexGrow(1.0, innerRow)
-        innerRow.addClassName(LumoUtility.Gap.XSMALL)
-        innerRow.addClassName(LumoUtility.Padding.XSMALL)
-        innerRow.width = "100%"
-        innerRow.style["flex-grow"] = "1"
-        middleColumn.setHeightFull()
-        innerRow.setFlexGrow(1.0, middleColumn)
-        middleColumn.addClassName(LumoUtility.Gap.XSMALL)
-        middleColumn.addClassName(LumoUtility.Padding.XSMALL)
-        middleColumn.width = "min-content"
-        middleColumn.style["flex-grow"] = "1"
-        pomFileSelectRadio.label = "Choose which pom files to update"
-        pomFileSelectRadio.width = "min-content"
-        pomFileSelectRadio.setItems("All in workspace", "Selection")
-        pomFileSelectRadio.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL)
-
-        customOrAutoDetectPrefixRadio.label = "Auto-detect or custom prefix"
-        customOrAutoDetectPrefixRadio.width = "min-content"
-        customOrAutoDetectPrefixRadio.setItems("Auto-detect", "Custom prefix")
-        customOrAutoDetectPrefixRadio.addThemeVariants(RadioGroupVariant.LUMO_VERTICAL)
-        customPrefixTextfield.label = "Prefix"
-        customPrefixTextfield.width = "100%"
-        rightColumn.setHeightFull()
-        innerRow.setFlexGrow(1.0, rightColumn)
-        rightColumn.addClassName(LumoUtility.Gap.XSMALL)
-        rightColumn.addClassName(LumoUtility.Padding.XSMALL)
-        rightColumn.width = "100%"
-        rightColumn.style["flex-grow"] = "1"
-
-        content!!.add(mainRow)
-        mainRow.add(leftColumn)
-        leftColumn.add(prefixPomsButton)
-        leftColumn.add(updateDependenciesButton)
-        mainRow.add(layoutColumn3)
-        layoutColumn3.add(h5)
-        layoutColumn3.add(innerRow)
-        innerRow.add(middleColumn)
-        middleColumn.add(pomFileSelectRadio)
-        middleColumn.add(customOrAutoDetectPrefixRadio)
-        middleColumn.add(customPrefixTextfield)
-        innerRow.add(rightColumn)
-        rightColumn.add(treeGrid)
-    }
 }
